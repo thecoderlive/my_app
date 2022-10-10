90bbb8d59efbe6f42fb6c458bb8352fa6f8e942b
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-10 13:26:59 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "ed5fd637-0712-467a-a45f-853fee9e6199",
+    "home_feed": [
+        {
+            "id": "337c16d7-48dd-45b9-88cc-ac056122fc3d",
+            "artist_name": "sport",
+            "home_carousel": [
+                {
+                    "id": "77257513-ea35-40dd-bb36-6f117ba70a86",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "03b3a8a3-fc82-4fbe-9a83-8bd97182b346",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png"
+                },
+                {
+                    "id": "0ff1b1cc-0980-4ac8-845a-e45e321ee2a9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                },
+                {
+                    "id": "bc3b392b-8bc5-419f-9b82-8b12604c806a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                },
+                {
+                    "id": "8f7a243d-dfd8-489a-86b0-7625121643cc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/240.png"
+                }
+            ],
+            "art_caption": "down",
+            "art_description": "When weight entire human. Finish bag responsibility traditional choice. Order nor art wrong well call base. Material option garden idea full answer."
+        },
+        {
+            "id": "a397c1e8-44c3-4b05-8a94-c1e522f548d7",
+            "artist_name": "can",
+            "home_carousel": [
+                {
+                    "id": "591c4493-8014-4e8f-9956-3f83b73fe78d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                },
+                {
+                    "id": "45557704-c0fc-4dcc-9766-5059d9516f3e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/52.png"
+                },
+                {
+                    "id": "e89ff6f4-2802-447d-9f12-855fa96db05a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "81f804d5-2bd2-4e61-b414-5516bd693a4f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/295.png"
+                },
+                {
+                    "id": "4bb49386-7dd4-4f41-9bc4-e2cfd4bcaa71",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                }
+            ],
+            "art_caption": "opportunity",
+            "art_description": "Home try though. May ahead not something term cut. Way billion area trouble be. Sign describe apply believe peace thousand step how."
+        },
+        {
+            "id": "4fbc06dd-417d-4826-8ebf-d497173f45f2",
+            "artist_name": "black",
+            "home_carousel": [
+                {
+                    "id": "945c1dec-31ed-413d-b48c-f819f9450fd9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/150.png"
+                },
+                {
+                    "id": "b79e9987-c565-4103-83da-f0e525b18aaa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+                },
+                {
+                    "id": "49746c5d-e503-45ee-ab16-36ab6327ee7f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "d810f9c8-7a49-43fe-a704-ebe3e4ea3ced",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/130.png"
+                },
+                {
+                    "id": "bbc58e7a-62e6-453b-9625-6d2889cc49e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png"
+                }
+            ],
+            "art_caption": "test",
+            "art_description": "Area fund security black man stop star. Seek section a."
+        },
+        {
+            "id": "e62dc5f3-534d-4a67-8def-5cf86ddac89c",
+            "artist_name": "evidence",
+            "home_carousel": [
+                {
+                    "id": "480e20e3-ffb8-4ce3-ac54-4e565f239b8f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                },
+                {
+                    "id": "a63f42e9-628c-4d12-aa10-dbcee332a1a0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "5c5ab2a0-81c1-427b-b4ad-1847d848b3d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+                },
+                {
+                    "id": "9682a75e-71f4-40fa-86b7-d814ad5045c8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "dd41c6e8-c176-434d-8967-327b9413ec14",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                }
+            ],
+            "art_caption": "those",
+            "art_description": "May reveal ago mention theory. Only and land."
+        },
+        {
+            "id": "760d2cd3-c649-418b-9a4f-949101603d21",
+            "artist_name": "fire",
+            "home_carousel": [
+                {
+                    "id": "0eae432c-0db2-4ecf-bcab-a082304344f7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "445a8411-b64a-4537-9326-0c838ce1700c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "a4ca516c-ddd2-4b0f-9060-cfe80da2c7c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "f06badc1-c839-44e7-9f4d-30082fad2ac0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/82.png"
+                },
+                {
+                    "id": "7d7ed6b2-4418-487b-abc6-79b4ba06f3d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/196.png"
+                }
+            ],
+            "art_caption": "student",
+            "art_description": "Stand civil player music important agent. Tough bed fund pick local decade four."
+        }
+    ]
+}
